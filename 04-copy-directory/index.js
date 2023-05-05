940fafcb8c61461ddd9ca086e20d7b6012762957
kotokatu <117390625+kotokatu@users.noreply.github.com>
2023-05-05 13:42:38 +0300

    feat: add solution for task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,29 @@
+const fs = require('fs');
+const path = require('path');
 
+const copyFolder = async () => {
+  const origDirPath = path.join(__dirname, 'files');
+  const newDirPath = path.join(__dirname, 'files-copy');
+  const origDirContent = await fs.promises.readdir(origDirPath);
+  try {
+    await fs.promises.mkdir(newDirPath, { recursive: false });
+  } catch (err) {
+    if (err.code === 'EEXIST') {
+      await fs.promises.rm(newDirPath, { recursive: true, force: true });
+      await fs.promises.mkdir(newDirPath);
+    } else {
+      console.error('The directory could not be created');
+    }
+  }
+  for (let i = 0; i < origDirContent.length; i++) {
+    const origFilePath = path.join(origDirPath, origDirContent[i]);
+    const newFilePath = path.join(newDirPath, origDirContent[i]);
+    try {
+      await fs.promises.copyFile(origFilePath, newFilePath);
+    } catch {
+      console.error('The file could not be copied');
+    }
+  }
+}
+
+copyFolder();
