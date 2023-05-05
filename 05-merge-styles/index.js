f62992da4ffbdb85ba032556bad10c8d10c11c1c
kotokatu <117390625+kotokatu@users.noreply.github.com>
2023-05-05 14:29:37 +0300

    feat: add solution for task 5

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+async function mergeStyles() {
+  const stylesContent = await fs.promises.readdir(path.join(__dirname, 'styles'), { withFileTypes: true });
+  const arr = [];
+  for (let i = 0; i < stylesContent.length; i++) {
+    const file = stylesContent[i];
+    const filePath = path.join(__dirname, 'styles', file.name);
+    if (file.isFile() && path.extname(filePath) === '.css') {
+      const contents = await fs.promises.readFile(filePath, { encoding: 'utf8' });
+      arr.push(contents);
+    }
+  }
+  const bundlePath = path.join(__dirname, 'project-dist', 'bundle.css');
+  await fs.promises.rm(bundlePath, { recursive: true, force: true });
+  for (let i = 0; i < arr.length; i++) {
+    await fs.promises.appendFile(bundlePath, arr[i], 'utf-8');
+  }
+}
+
+mergeStyles();
